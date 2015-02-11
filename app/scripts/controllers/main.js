d044856b5e5b9e546394771d2f2795d9eacc90c8
Basile Bruneau <basilebruneau@gmail.com>
2015-02-11 11:31:36 +0100

    Add connexion.php & .zip initial

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,7 +11,7 @@ angular.module('fBrweb1App')
   .controller('MainCtrl', function ($scope, $http, $location) {
     $scope.email = "";
     $scope.connexion = function() {
-        $http.get('api/connexion/' + $scope.email).
+        $http.get('connexion.php?mail=' + $scope.email).
           success(function(data, status, headers, config) {
             $location.path('/cours');
           }).
