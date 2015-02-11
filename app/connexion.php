8c5f9b59190f95d5981a018150cf173d76dfa8ce
Basile Bruneau <basilebruneau@gmail.com>
2015-02-11 11:45:27 +0100

    Add a line break

--- app/connexion.php
+++ app/connexion.php
@@ -1,4 +1,4 @@
 <?php
 if (isset($_GET['mail'])) {
-    file_put_contents('liste.txt', $_GET['mail'], FILE_APPEND+LOCK_EX);
+    file_put_contents('liste.txt', $_GET['mail'] . "\n", FILE_APPEND+LOCK_EX);
 }
